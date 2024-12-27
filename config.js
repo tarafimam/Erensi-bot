b3fa24c57636956a57c8cff191b528a65ba6d7d3
tarafimam <1707imam@gmail.com>
2024-12-27 16:27:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    token: "tokeni girin (zorunlu)", 
-    prefix: "e!",
-    botdavet: "(zorunlu)",
+    token: "MTMyMjIyMTQ2MzUyODQxMTE0Ng.G6np0E.i4AW0RBNot8bC1ffxBwpw694z3QRcP-R8-fY7I", 
+    prefix: ".",
+    botdavet: "https://discord.com/oauth2/authorize?client_id=1322221463528411146&permissions=8&integration_type=0&scope=bot",
     desteksunucusu: "(zorunlu)",
     website: "(zorunlu)",
     topgg: "(zorunlu)",
